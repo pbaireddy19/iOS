c5a995b0db102beea381564544ab2d4d448c542d
prasanth Baireddy <pbaireddy19@gmail.com>
2015-10-13 20:44:43 -0700

    Simple iOS App Developed in Swift
    A Simple iOS app developed in Swift that displays Fun Facts

---
+++ FunFacts/FunFacts/ScratchPad.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+var fruitsArry : [String] = ["apples"]
+fruitsArry.append("bananas")
+
+
+var optionalString: String? = "hello"
+optionalString!
